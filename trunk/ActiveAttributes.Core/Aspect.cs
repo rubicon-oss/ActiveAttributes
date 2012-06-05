a6861bf34d5fb0274b2a25e0475ec125b2a0a131
matkoch <none@none>
2012-06-05 16:41:22 +0200

    Added Priority, Serialization

--- trunk/ActiveAttributes.Core/Aspect.cs
+++ trunk/ActiveAttributes.Core/Aspect.cs
@@ -1,15 +1,42 @@
 ﻿using System;
+using System.Runtime.Serialization;
+using System.Security.Permissions;
 
 namespace ActiveAttributes.Core
 {
+  [Serializable]
   [AttributeUsage (AttributeTargets.Method, AllowMultiple = true)]
-  public class Aspect : Attribute
+  public class Aspect : Attribute, ISerializable
   {
+    public Aspect ()
+    {
+    }
+
+    protected Aspect (SerializationInfo info, StreamingContext context)
+    {
+      Scope = (AspectScope) info.GetInt32 ("scope");
+      Priority = info.GetInt32 ("priority");
+    }
+
+    [SecurityPermissionAttribute (SecurityAction.Demand, SerializationFormatter = true)]
+    public void GetObjectData (SerializationInfo info, StreamingContext context)
+    {
+      info.AddValue ("scope", (int) Scope);
+      info.AddValue ("priority", Priority);
+    }
+
     public AspectScope Scope { get; set; }
+
+    public int Priority { get; set; }
+
+    public object Clone ()
+    {
+      return MemberwiseClone ();
+    }
 
     public virtual void OnInvoke (Invocation invocation)
     {
-      invocation.Proceed ();
+      invocation.Proceed();
     }
   }
 }
--- trunk/ActiveAttributes.UnitTests/AspectTest.cs
+++ trunk/ActiveAttributes.UnitTests/AspectTest.cs
@@ -1,8 +1,7 @@
-﻿// Copyright (C) 2005 - 2009 rubicon informationstechnologie gmbh
-// All rights reserved.
-//
-
-using System;
+﻿using System;
+using System.IO;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
 using ActiveAttributes.Core;
 using NUnit.Framework;
 
@@ -23,12 +22,50 @@ namespace ActiveAttributes.UnitTests
       Assert.That (called, Is.True);
     }
 
-    class TestableAspect : Aspect
+    [Test]
+    public void Clone ()
     {
+      var aspect = new TestableAspect { Scope = AspectScope.Instance };
+      var copy = (Aspect) aspect.Clone();
+
+      Assert.That (copy.Scope, Is.EqualTo (AspectScope.Instance));
+    }
+
+    [Test]
+    public void Serialize ()
+    {
+      var aspect = new TestableAspect
+      {
+        Scope = AspectScope.Instance,
+        Priority = 10
+      };
+      var formatter = new BinaryFormatter();
+      var memoryStream = new MemoryStream();
+
+      formatter.Serialize (memoryStream, aspect);
+      memoryStream.Position = 0;
+      var copy = (Aspect) formatter.Deserialize (memoryStream);
+
+      Assert.That (copy.Scope, Is.EqualTo (aspect.Scope));
+      Assert.That (copy.Priority, Is.EqualTo (aspect.Priority));
+    }
+
+    [Serializable]
+    private class TestableAspect : Aspect
+    {
+      public TestableAspect ()
+      {
+      }
+
+      protected TestableAspect (SerializationInfo info, StreamingContext context)
+          : base (info, context)
+      {
+      }
+
       public override void OnInvoke (Invocation invocation)
       {
         base.OnInvoke (invocation);
-      } 
+      }
     }
   }
 }
