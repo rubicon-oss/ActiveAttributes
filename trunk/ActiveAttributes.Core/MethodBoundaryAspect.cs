d0a6bb2b60b03656457058c42aff27a671470271
matkoch <none@none>
2012-06-05 13:14:56 +0200

    Added MethodBoundaryAspect

---
+++ trunk/ActiveAttributes.Core/MethodBoundaryAspect.cs
@@ -0,0 +1,35 @@
+﻿// Copyright (C) 2005 - 2009 rubicon informationstechnologie gmbh
+// All rights reserved.
+//
+
+using System;
+
+namespace ActiveAttributes.Core
+{
+  public class MethodBoundaryAspect : Aspect
+  {
+    public override void OnInvoke (Invocation invocation)
+    {
+      OnEntry();
+
+      try
+      {
+        base.OnInvoke (invocation);
+        OnSuccess ();
+      }
+      catch (Exception)
+      {
+        OnException ();
+      }
+      finally
+      {
+        OnExit();
+      }
+    }
+
+    public virtual void OnEntry () { }
+    public virtual void OnExit () { }
+    public virtual void OnException () { }
+    public virtual void OnSuccess () { }
+  }
+}
---
+++ trunk/ActiveAttributes.UnitTests/MethodBoundaryAspectTest.cs
@@ -0,0 +1,112 @@
+﻿// Copyright (C) 2005 - 2009 rubicon informationstechnologie gmbh
+// All rights reserved.
+//
+
+using System;
+using ActiveAttributes.Core;
+using NUnit.Framework;
+
+namespace ActiveAttributes.UnitTests
+{
+  [TestFixture]
+  public class MethodBoundaryAspectTest
+  {
+    private TestableMethodBoundaryAspect _obj;
+    private Invocation _throwingInvocation;
+    private Invocation _blankInvocation;
+
+    [SetUp]
+    public void SetUp ()
+    {
+      _obj = new TestableMethodBoundaryAspect();
+
+      _throwingInvocation = new Invocation (new Action (() => { throw new Exception(); }));
+      _blankInvocation = new Invocation (new Action (() => { }));
+    }
+
+    [Test]
+    public void OnEntry ()
+    {
+      var invocation = new Invocation (new Action (() => Assert.That (_obj.OnEntryCalled, Is.True)));
+
+      _obj.OnInvoke (invocation);
+    }
+
+    [Test]
+    public void OnExit ()
+    {
+      var invocation = new Invocation (new Action (() => Assert.That (_obj.OnExitCalled, Is.False)));
+
+      _obj.OnInvoke (invocation);
+
+      Assert.That (_obj.OnExitCalled, Is.True);
+    }
+
+    [Test]
+    public void OnExit_WithException ()
+    {
+      try
+      {
+        _obj.OnInvoke (_throwingInvocation);
+      }
+      catch
+      {
+      }
+
+      Assert.That (_obj.OnExitCalled, Is.True);
+    }
+
+    [Test]
+    public void OnException_WithException ()
+    {
+      try
+      {
+        _obj.OnInvoke (_throwingInvocation);
+      }
+      catch (Exception)
+      {
+      }
+
+      Assert.That (_obj.OnExceptionCalled, Is.True);
+    }
+
+    [Test]
+    public void OnException_WithoutException ()
+    {
+      _obj.OnInvoke (_blankInvocation);
+
+      Assert.That (_obj.OnExceptionCalled, Is.False);
+    }
+
+    [Test]
+    public void OnSuccess_WithException ()
+    {
+      _obj.OnInvoke (_throwingInvocation);
+
+      Assert.That (_obj.OnSuccessCalled, Is.False);
+    }
+
+    [Test]
+    public void OnSuccess_WithoutException ()
+    {
+      _obj.OnInvoke (_blankInvocation);
+
+      Assert.That (_obj.OnSuccessCalled, Is.True);
+    }
+
+    class TestableMethodBoundaryAspect : MethodBoundaryAspect
+    {
+      public bool OnEntryCalled { get; private set; }
+      public override void OnEntry () { OnEntryCalled = true; }
+
+      public bool OnExitCalled { get; private set; }
+      public override void OnExit () { OnExitCalled = true; }
+
+      public bool OnExceptionCalled { get; private set; }
+      public override void OnException () { OnExceptionCalled = true; }
+
+      public bool OnSuccessCalled { get; private set; }
+      public override void OnSuccess () { OnSuccessCalled = true; }
+    }
+  }
+}
