ef32ebf9fd38ab6b554952ef3a37cda4cbe1dd3f
matkoch <none@none>
2012-06-29 02:24:11 +0200

    Added init of delegate and instance aspects (partly)

--- trunk/ActiveAttributes.Core/Assembly/ConstructorPatcher.cs
+++ trunk/ActiveAttributes.Core/Assembly/ConstructorPatcher.cs
@@ -6,6 +6,10 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
+
+using ActiveAttributes.Core.Aspects;
+using ActiveAttributes.Core.Extensions;
+
 using Microsoft.Scripting.Ast;
 using Remotion.TypePipe.MutableReflection;
 using Remotion.TypePipe.MutableReflection.BodyBuilding;
@@ -14,22 +18,59 @@ namespace ActiveAttributes.Core.Assembly
 {
   public class ConstructorPatcher
   {
-    public void Patch (FieldIntroducer.Data fieldData, IEnumerable<CompileTimeAspect> aspects, MutableMethodInfo mutableMethod)
+    public void Patch (FieldIntroducer.Data fieldData, IEnumerable<CompileTimeAspect> compileTimeAspects, MutableMethodInfo mutableMethod,
+      MutableMethodInfo copiedMethod)
     {
       var mutableType = ((MutableType) mutableMethod.DeclaringType);
       var mutableConstructor = mutableType.AllMutableConstructors.Single();
+
 
 
       mutableConstructor.SetBody (
           ctx =>
           Expression.Block (
               ctx.PreviousBody,
-              GetMethodInfoAssignExpression (fieldData, mutableMethod, ctx)));
+              GetMethodInfoAssignExpression (fieldData.MethodInfoField, mutableMethod, ctx),
+              GetDelegateAssignExpression (fieldData.DelegateField, mutableMethod, ctx, copiedMethod),
+              GetInstanceAspectAssignExpression (fieldData.InstanceAspectsField, compileTimeAspects, ctx)));
     }
 
-    private Expression GetMethodInfoAssignExpression (FieldIntroducer.Data fieldData, MutableMethodInfo mutableMethod, BodyContextBase ctx)
+    private Expression GetInstanceAspectAssignExpression (FieldInfo instanceAspectsField, IEnumerable<CompileTimeAspect> compileTimeAspects, ConstructorBodyModificationContext ctx)
     {
-      var methodInfoFieldExpression = Expression.Field (ctx.This, fieldData.MethodInfoField);
+      var instanceAspectsFieldExpression = Expression.Field (ctx.This, instanceAspectsField);
+      var instanceAspectsInitExpression = Expression.NewArrayInit (typeof (AspectAttribute),
+        GetInstanceAspectsInitExpressions (compileTimeAspects));
+      var instanceAspectsAssignExpression = Expression.Assign (instanceAspectsFieldExpression, instanceAspectsInitExpression);
+      return instanceAspectsAssignExpression;
+    }
+
+    private IEnumerable<Expression> GetInstanceAspectsInitExpressions (IEnumerable<CompileTimeAspect> compileTimeAspects)
+    {
+      return compileTimeAspects.Select (x => Expression.New (x.ConstructorInfo)).Cast<Expression>();
+    }
+
+    private Expression GetDelegateAssignExpression (FieldInfo delegateField, MutableMethodInfo mutableMethod, ConstructorBodyModificationContext ctx, MutableMethodInfo copiedMethod)
+    {
+      var delegateFieldExpression = Expression.Field (ctx.This, delegateField);
+      var delegateType = mutableMethod.GetDelegateType ();
+      var createDelegateMethodInfo = typeof (Delegate).GetMethod (
+          "CreateDelegate",
+          new[] { typeof (Type), typeof (object), typeof (MethodInfo) });
+      var delegateCreateExpression = Expression.Call (
+          null,
+          createDelegateMethodInfo,
+          Expression.Constant (delegateType),
+          ctx.This,
+          Expression.Constant (copiedMethod, typeof (MethodInfo)));
+      var delegateConvertExpression = Expression.Convert (delegateCreateExpression, delegateField.FieldType);
+      var delegateAssignExpression = Expression.Assign (delegateFieldExpression, delegateConvertExpression);
+
+      return delegateAssignExpression;
+    }
+
+    private Expression GetMethodInfoAssignExpression (FieldInfo methodInfoField, MutableMethodInfo mutableMethod, BodyContextBase ctx)
+    {
+      var methodInfoFieldExpression = Expression.Field (ctx.This, methodInfoField);
       var methodInfoConstantExpression = Expression.Constant (mutableMethod, typeof (MethodInfo));
       var methodInfoAssignExpression = Expression.Assign (methodInfoFieldExpression, methodInfoConstantExpression);
 
--- trunk/ActiveAttributes.UnitTests/Assembly/ConstructorPatcherTest.cs
+++ trunk/ActiveAttributes.UnitTests/Assembly/ConstructorPatcherTest.cs
@@ -1,8 +1,12 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Reflection;
 using ActiveAttributes.Core.Aspects;
 using ActiveAttributes.Core.Assembly;
+
+using Microsoft.Scripting.Ast;
+
 using NUnit.Framework;
 using Remotion.Utilities;
 
@@ -12,23 +16,58 @@ namespace ActiveAttributes.UnitTests.Assembly
   public class ConstructorPatcherTest : TestBase
   {
     private ConstructorPatcher _patcher;
+    private MethodInfo _methodInfo;
+    private MethodInfo _copiedMethodInfo;
 
     [SetUp]
     public override void SetUp ()
     {
       _patcher = new ConstructorPatcher();
+      _methodInfo = MemberInfoFromExpressionUtility.GetMethod (((DomainType obj) => obj.Method ()));
+      _copiedMethodInfo = MemberInfoFromExpressionUtility.GetMethod (((DomainType obj) => obj.Method_Copy ()));
     }
 
     [Test]
     public void Init_MethodInfo ()
     {
-      var methodInfo = MemberInfoFromExpressionUtility.GetMethod (((DomainType obj) => obj.Method()));
-      var instance = CreateInstance<DomainType> (new CompileTimeAspect[0], methodInfo);
+      var instance = CreateInstance<DomainType> (new CompileTimeAspect[0], _methodInfo, _copiedMethodInfo);
 
-      Assert.That (instance._m_Method1_MethodInfo, Is.EqualTo (methodInfo));
+      Assert.That (instance._m_Method1_MethodInfo, Is.EqualTo (_methodInfo));
     }
 
-    private T CreateInstance<T> (IEnumerable<CompileTimeAspect> aspects, MethodInfo methodInfo) where T: DomainTypeBase
+    [Test]
+    public void Init_Delegate ()
+    {
+      var instance = CreateInstance<DomainType> (new CompileTimeAspect[0], _methodInfo, _copiedMethodInfo);
+
+      Assert.That (instance._m_Method1_Delegate, Is.EqualTo (new Action (instance.Method_Copy)));
+    }
+
+    [Test]
+    public void Init_InstanceAspects_Single ()
+    {
+      var compileTimeAspects = GetCompileTimeAspects (_methodInfo);
+      var instance = CreateInstance<DomainType> (compileTimeAspects, _methodInfo, _copiedMethodInfo);
+
+      Assert.That (instance._m_Method1_InstanceAspects, Is.Not.Null);
+      Assert.That (instance._m_Method1_InstanceAspects, Has.Length.EqualTo (1));
+      Assert.That (instance._m_Method1_InstanceAspects, Has.All.InstanceOf<DomainAspectAttribute> ());
+    }
+
+    [Test]
+    public void Init_InstanceAspects_Multiple ()
+    {
+      var methodInfo = MemberInfoFromExpressionUtility.GetMethod (((DomainType obj) => obj.Method2 ()));
+      var compileTimeAspects = GetCompileTimeAspects (methodInfo);
+      var instance = CreateInstance<DomainType> (compileTimeAspects, methodInfo, _copiedMethodInfo);
+
+      Assert.That (instance._m_Method1_InstanceAspects, Is.Not.Null);
+      Assert.That (instance._m_Method1_InstanceAspects, Has.Length.EqualTo (2));
+      Assert.That (instance._m_Method1_InstanceAspects, Has.All.InstanceOf<DomainAspectAttribute> ());
+    }
+
+    private T CreateInstance<T> (IEnumerable<CompileTimeAspect> aspects, MethodInfo methodInfo, MethodInfo copiedMethod)
+      where T: DomainTypeBase
     {
       var methodInfoField = MemberInfoFromExpressionUtility.GetField (((T obj) => obj._m_Method1_MethodInfo));
       var delegateField = MemberInfoFromExpressionUtility.GetField (((T obj) => obj._m_Method1_Delegate));
@@ -46,25 +85,43 @@ namespace ActiveAttributes.UnitTests.Assembly
           mutableType =>
           {
             var mutableMethod = mutableType.GetOrAddMutableMethod (methodInfo);
-            _patcher.Patch (fieldData, aspects, mutableMethod);
+            var mutableCopy = mutableType.GetOrAddMutableMethod (copiedMethod);
+
+            _patcher.Patch (fieldData, aspects, mutableMethod, mutableCopy);
           });
 
       return (T) Activator.CreateInstance (type);
     }
 
+    private CompileTimeAspect[] GetCompileTimeAspects (MethodInfo methodInfo)
+    {
+      var customAttributeData = CustomAttributeData.GetCustomAttributes (methodInfo);
+      var compileTimeAspects = customAttributeData.Select (x => new CompileTimeAspect (x)).ToArray();
+      return compileTimeAspects;
+    }
+
     public class DomainTypeBase
     {
       public MethodInfo _m_Method1_MethodInfo;
-      public Action<DomainType> _m_Method1_Delegate;
+      public Action _m_Method1_Delegate;
       public AspectAttribute[] _s_Method1_StaticAspects;
       public AspectAttribute[] _m_Method1_InstanceAspects;
     }
 
     public class DomainType : DomainTypeBase
     {
-      public void Method ()
-      {
-      }
+      [DomainAspect]
+      public virtual void Method () { }
+
+      public virtual void Method_Copy () { }
+
+      [DomainAspect]
+      [DomainAspect]
+      public virtual void Method2 () { }
+    }
+
+    public class DomainAspectAttribute : AspectAttribute
+    {
     }
   }
 }
