3867103e923682dac31e0e7e6a057f51087ef567
matkoch <none@none>
2012-07-02 17:23:02 +0200

    Added simple GetDelegateType overload

--- trunk/ActiveAttributes.Core/Extensions/MethodInfoExtensions.cs
+++ trunk/ActiveAttributes.Core/Extensions/MethodInfoExtensions.cs
@@ -22,6 +22,15 @@ namespace ActiveAttributes.Core.Extensions
 
       var delegateTypes = instanceType2.Concat (parameterTypes).Concat (returnType).ToArray ();
       return Expression.GetDelegateType (delegateTypes);
+    }
+
+    public static Type GetDelegateType (this MethodInfo methodInfo)
+    {
+      var parameterTypes = methodInfo.GetParameters ().Select (x => x.ParameterType);
+      var returnType = new[] { methodInfo.ReturnType };
+
+      var delegateTypes = parameterTypes.Concat (returnType).ToArray ();
+      return Expression.GetDelegateType (delegateTypes);
     } 
   }
 }
