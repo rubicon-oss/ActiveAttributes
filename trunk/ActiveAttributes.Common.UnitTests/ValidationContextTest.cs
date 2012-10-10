17e7851d892176defa773480f1a95236e04283ac
matthias.koch@W0396.int.rubicon-it.com <matthias.koch@W0396.int.rubicon-it.com>
2012-10-10 22:03:38 +0200

    Added ValidationContextTest

---
+++ trunk/ActiveAttributes.Common.UnitTests/ValidationContextTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Reflection;
+using ActiveAttributes.Core.Contexts;
+using NUnit.Framework;
+
+namespace ActiveAttributes.Common.UnitTests
+{
+  [TestFixture]
+  public class ValidationContextTest
+  {
+    [Test]
+    public void Initialization ()
+    {
+      var method = (MethodInfo) MethodInfo.GetCurrentMethod();
+      var invocationContext = new FuncInvocationContext<ValidationContextTest, string, int> (method, this, "123") { ReturnValue = 12 };
+
+      var validationContext = new ValidationContext (invocationContext);
+
+      Assert.That (validationContext.Method, Is.SameAs (method));
+      Assert.That (validationContext.Instance, Is.SameAs (this));
+      Assert.That (validationContext.Arguments, Is.EquivalentTo (new[] { "123" }));
+      Assert.That (validationContext.ReturnValue, Is.EqualTo (12));
+    }
+  }
+}
