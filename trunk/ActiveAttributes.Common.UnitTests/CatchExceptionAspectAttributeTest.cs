39a67c33439582ace18c313077867fd049c0a35d
matthias.koch@W0396.int.rubicon-it.com <matthias.koch@W0396.int.rubicon-it.com>
2012-10-10 20:46:12 +0200

    Added CatchExceptionAspectAttribute

---
+++ trunk/ActiveAttributes.Common.UnitTests/CatchExceptionAspectAttributeTest.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using ActiveAttributes.Core;
+using NUnit.Framework;
+
+namespace ActiveAttributes.Common.UnitTests
+{
+  [TestFixture]
+  public class CatchExceptionAspectAttributeTest
+  {
+    [Test]
+    public void CatchesException ()
+    {
+      var instance = ObjectFactory.Create<DomainType>();
+
+      Assert.That (() => instance.CatchableThrow1 (), Throws.Nothing);
+      Assert.That (() => instance.CatchableThrow2 (), Throws.Nothing);
+    }
+
+    [Test]
+    public void RethrowsException ()
+    {
+      var instance = ObjectFactory.Create<DomainType>();
+
+      Assert.That (() => instance.UncatchableThrow (), Throws.Exception);
+    }
+
+    [Test]
+    public void Proceeds ()
+    {
+      var instance = ObjectFactory.Create<DomainType>();
+
+      instance.Method();
+
+      Assert.That (instance.MethodExecuted, Is.True);
+    }
+
+    public class DomainType
+    {
+      [CatchExceptionAspect]
+      public virtual void CatchableThrow1 ()
+      {
+        throw new Exception();
+      }
+
+      [CatchExceptionAspect(typeof(Exception))]
+      public virtual void CatchableThrow2 ()
+      {
+        throw new InvalidOperationException ();
+      }
+
+      [CatchExceptionAspect(typeof(InvalidOperationException))]
+      public virtual void UncatchableThrow ()
+      {
+        throw new Exception ();
+      }
+
+      public bool MethodExecuted { get; private set; }
+
+      [CatchExceptionAspect]
+      public virtual void Method ()
+      {
+        MethodExecuted = true;
+      }
+    }
+  }
+}
---
+++ trunk/ActiveAttributes.Common/CatchExceptionAspectAttribute.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using ActiveAttributes.Core.Aspects;
+using ActiveAttributes.Core.Invocations;
+
+namespace ActiveAttributes.Common
+{
+  /// <summary>
+  /// Catches exceptions of a certain type (e.g. <see cref="Exception"/> unless otherwise stated).
+  /// </summary>
+  public class CatchExceptionAspectAttribute : MethodInterceptionAspectAttribute
+  {
+    private readonly Type _exceptionType;
+
+    public CatchExceptionAspectAttribute (Type exceptionType = null)
+    {
+      _exceptionType = exceptionType ?? typeof (Exception);
+    }
+
+    public override void OnIntercept (IInvocation invocation)
+    {
+      try
+      {
+        invocation.Proceed();
+      }
+      catch (Exception exception)
+      {
+        if (!_exceptionType.IsInstanceOfType(exception))
+          throw;
+      }
+    }
+  }
+}
