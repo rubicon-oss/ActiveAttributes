982a8e82944612776ec0b46d7171f1cae4b76086
matkoch <none@none>
2012-11-01 19:27:46 +0100

    Added ObjectMother

---
+++ trunk/ActiveAttributes.UnitTests/ObjectMother/ObjectMother.GetType.cs
@@ -0,0 +1,29 @@
+﻿// Copyright (c) rubicon IT GmbH, www.rubicon.eu
+//
+// See the NOTICE file distributed with this work for additional information
+// regarding copyright ownership.  rubicon licenses this file to you under 
+// the Apache License, Version 2.0 (the "License"); you may not use this 
+// file except in compliance with the License.  You may obtain a copy of the 
+// License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software 
+// distributed under the License is distributed on an "AS IS" BASIS, WITHOUT 
+// WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the 
+// License for the specific language governing permissions and limitations
+// under the License.
+using System;
+
+namespace ActiveAttributes.UnitTests
+{
+  public static partial class ObjectMother2
+  {
+    private static readonly Type[] s_types = new[] { typeof (object), typeof (int), typeof(void) };
+
+    public static Type GetType_ ()
+    {
+      return GetRandom (s_types);
+    }
+  }
+}
